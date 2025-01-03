5dea869a0e6bffc24f6e55b74ab4efd7ed627031
Kelvin Nilsen <kdnilsen@amazon.com>
2025-01-03 23:27:55 +0000

    Always cancel Extremem if any thread is terminated

--- Extremem/src/main/java/com/amazon/corretto/benchmark/extremem/ExtrememUncaughtExceptionHandler.java
+++ Extremem/src/main/java/com/amazon/corretto/benchmark/extremem/ExtrememUncaughtExceptionHandler.java
@@ -14,10 +14,11 @@ class ExtrememUncaughtExceptionHandler implements
   public void uncaughtException(Thread t, Throwable e) {
     try {
       Util.internalError("Thread " + t + ": terminated with uncaught exception: " + e);
-      if (e.getClass() == Class.forName("java.lang.OutOfMemoryError")) {
-        Util.severeInternalError();
-      }
-    } catch (Throwable x) {
+    } finally {
+      // Whenever any thread that is part of the Extremem workload is terminated, we should cancel
+      // the simulation.  Otherwise, Extremem will report percentile latencies and the GC log will
+      // report GC behaviors that do not truthfully represent the workload that was configured because
+      // the workload has been reduced by the termination of certain worker threads.
       Util.severeInternalError();
     }
   }
